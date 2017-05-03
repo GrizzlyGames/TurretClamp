528b1b9bbcc6052ac2ae8591bf06d5ee9fd84b77
GrizzlyGames <info@grizzlygames.co>
2017-05-03 18:56:09 -0400

    Root

---
+++ TurretClamp.21/clamp target fire alpha/Assets/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public bool quitBool ;
+
+	void Start () {
+		quitBool = false;
+
+
+	}
+	void Update () {
+		if (quitBool == true) {
+			Application.Quit ();
+			quitBool = false;
+		} else {
+			quitBool = false;
+		}
+
+
+
+	}
+
+	public void ChangeToScene (int ChangingScene){
+			SceneManager.LoadScene (ChangingScene);
+	}
+	public void Quitter(){
+		quitBool = true;
+	}
+}
---
+++ TurretClamp.21/clamp target fire alpha/Assets/resetCurdor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetCurdor : MonoBehaviour {
+
+
+	public Texture2D CursorTexture;
+	CursorMode curMode = CursorMode.Auto;
+	public Vector2 hotSpot =Vector2.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		Cursor.SetCursor (CursorTexture, hotSpot, curMode);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
