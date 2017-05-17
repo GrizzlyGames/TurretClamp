8fb594eebb1813a3981ef9406fcf2eca90b6f234
GrizzlyGamesCo <info@grizzlygames.co>
2017-05-17 13:27:27 -0400

    Update Main Menu

--- TurretClamp.21/UnityRoot/Assets/ChangeScene.cs
+++ TurretClamp.21/UnityRoot/Assets/ChangeScene.cs
@@ -5,29 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	public bool quitBool ;
-
-	void Start () {
-		quitBool = false;
-
-
-	}
-	void Update () {
-		if (quitBool == true) {
-			Application.Quit ();
-			quitBool = false;
-		} else {
-			quitBool = false;
-		}
-
-
-
-	}
-
-	public void ChangeToScene (int ChangingScene){
+    public void ChangeToScene (int ChangingScene){
 			SceneManager.LoadScene (ChangingScene);
 	}
+
 	public void Quitter(){
-		quitBool = true;
-	}
+        Application.Quit();
+    }
 }
