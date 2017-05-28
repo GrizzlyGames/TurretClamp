0101cd5a44cb553fe3f06c798176ec87cd1eb1e2
GrizzlyGamesCo <info@grizzlygames.co>
2017-05-27 21:33:51 -0400

    Camera Update

--- TurretClamp.21/UnityRoot/Assets/_MyAssets/Script/Player/Camera_Controller.cs
+++ TurretClamp.21/UnityRoot/Assets/_MyAssets/Script/Player/Camera_Controller.cs
@@ -4,29 +4,60 @@ using System.Collections;
 public class Camera_Controller : MonoBehaviour
 {
     public Transform target;
-    public float rotationYOffSet;
+    public Transform yTrans;
+    public Transform xTrans;
+    public Transform zTrans;
+
+
+    private float yRotation;
+    private float xRotation = 45;
+    private float zoom;
+
+    private void Start()
+    {
+        zoom = zTrans.localPosition.z;
+    }
 
     private void Update()
-    {
+    {        
         CameraFollow();
-        CameraRotate(); 
+        YRotate();
+        Zoom();
     }
 
-    private void CameraRotate()
+    private void YRotate()
     {
         if (Input.GetKey(KeyCode.A))
-        {
-            rotationYOffSet -= 1;
-            this.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, transform.rotation.y + rotationYOffSet, 0);
-        }
+            yRotation -= 1;
         else if (Input.GetKey(KeyCode.D))
+            yRotation += 1;
+        yTrans.rotation = Quaternion.Euler(0, transform.rotation.y + yRotation, 0);
+    }
+    private void Zoom()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            rotationYOffSet += 1;
-            this.transform.GetChild(0).transform.rotation = Quaternion.Euler(0, transform.rotation.y + rotationYOffSet, 0);
+            if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward scroll
+            {
+                if (xRotation < 89) 
+                    xRotation += 5f;
+                if(zoom > -3)
+                zoom -= 0.25f;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0) // back scroll
+            {
+                if (xRotation > 5)
+                    xRotation -= 5f;
+                if (zoom < 1)
+                    zoom += 0.25f;
+            }
+            xTrans.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            zTrans.localPosition = new Vector3(0, 0, zoom);            
         }
     }
+
     private void CameraFollow()
     {
-        this.transform.position = target.position;   
+        this.transform.position = target.position;
     }
 }
